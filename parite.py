27e924f2f75000d96aa8d332410abe183b0c6563
Robin Muller <robin.muller.romutech@gmail.com>
2020-02-15 03:09:58 +0100

    wip

--- parite.py
+++ parite.py
@@ -1,8 +1,17 @@
 #! /usr/bin/env python3
 # coding: utf-8
 
+import analysis.xml as x_an
+import analysis.csv as c_an
+
+import os
+
+def launch_analysis(data_file):
+    directory = os.path.dirname(os.path.dirname(__file__)) # we get the right path.
+
 def main():
-    pass
+    c_an.launch_analysis('current_mps.csv')
+    x_an.launch_analysis('SyceronBrut.xml')
 
 if __name__ == "__main__":
     main()
